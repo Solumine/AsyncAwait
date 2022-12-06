54b16170b55b5043f182fa96125ab97bd8858c18
Solumine <olivierbalmet@gmail.com>
2022-12-06 22:01:29 +0100

    envoi

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await DoMyTask();
+
+        async Task Test1()
+        {
+            var s = new Stopwatch();
+            s.Start();
+            await Task.Delay(1500);
+            s.Stop();
+            Console.WriteLine($"\nTest 1: {s.ElapsedMilliseconds} ms");
+        }
+
+        async Task Test2()
+        {
+            var s = new Stopwatch();
+            s.Start();
+            await Task.Delay(1500);
+            s.Stop();
+            Console.WriteLine($"\nTest 2: {s.ElapsedMilliseconds} ms");
+        }
+
+        async Task Test3()
+        {
+            var s = new Stopwatch();
+            s.Start();
+            await Task.Delay(1500);
+            s.Stop();
+            Console.WriteLine($"\nTest 3: {s.ElapsedMilliseconds} ms");
+        }
+
+        async Task DoMyTask()
+        {
+            var s = new Stopwatch();
+            s.Start();
+            var t1 = Test1();
+            var t2 = Test2();
+            var t3 = Test3();
+            await Task.WhenAll(t1, t2, t3);
+            s.Stop();
+            Console.WriteLine($"\nDelay: {s.ElapsedMilliseconds} ms");
+        }
+    }
+}
